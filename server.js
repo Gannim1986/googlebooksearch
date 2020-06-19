030a13be55536cc701a39fc560dc6414427e4775
Gannim1986 <36219943+Gannim1986@users.noreply.github.com>
2020-06-19 16:38:04 -0700

    made changes to server.js

--- server.js
+++ server.js
@@ -19,11 +19,7 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 
 // Start the API server
 app.listen(PORT, () =>
